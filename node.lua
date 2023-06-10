791265a27e22c73970535d11e6e4cfe56969c4fa
Arpit Sharma <Sharma.Arpit956@Gmail.com>
2023-06-10 21:20:39 +0100

    i

--- node.lua
+++ node.lua
@@ -638,6 +638,11 @@ function node.render()
     Queue.tick()
 end
 
+node.event("input", function(line, client)
+    node.client_write(client, current_asset_id)
+end)
+
+
 local clients = {}
 node.event("connect", function(client, path)
     clients[client] = true -- add to list of connected clients
