d975bb429c5d1249d63990b75a14c022d9bb5913
Arpit Sharma <Sharma.Arpit956@Gmail.com>
2023-06-11 17:22:13 +0100

    pu out removed

--- main.py
+++
@@ -1,9 +0,0 @@
-import mqtt_helper
-import ibquery
-
-ib = ibquery.InfoBeamerQuery()
-s = ib.node('root').io(raw=True)
-asset_name= s.readline()
-print(asset_name) # this should read the value sent in client_write above
-print(mqtt_helper.send_data_to_adafruit_io('current-asset-name', str(asset_name)))
-
